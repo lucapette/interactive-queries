60259bc4706967c9c04a4f063ed95560daaea59c
lucapette <ciao@lucapette.me>
2022-03-31 22:16:11 +0200

    Producing works

--- src/main/kotlin/me/lucapette/interactivequeries/InteractiveQueriesApplication.kt
+++ src/main/kotlin/me/lucapette/interactivequeries/InteractiveQueriesApplication.kt
@@ -1,10 +1,22 @@
 package me.lucapette.interactivequeries
 
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class InteractiveQueriesApplication
+class InteractiveQueriesApplication {
+    @Bean
+    fun producer(): KafkaProducer<String, String> {
+        return KafkaProducer(mapOf(
+            "bootstrap.servers" to "localhost:9092",
+            "key.serializer" to StringSerializer::class.java.name,
+            "value.serializer" to  StringSerializer::class.java.name,
+        ))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<InteractiveQueriesApplication>(*args)
